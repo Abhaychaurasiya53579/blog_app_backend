4d6798dd6563b3dc72f9631285c52a75f683fb41
Abhaychaurasiya53579 <97787240+Abhaychaurasiya53579@users.noreply.github.com>
2023-08-18 20:47:04 +0530

    changes

--- routers/blog_route.js
+++ routers/blog_route.js
@@ -10,14 +10,15 @@ const response =(res,status,result)=>{
 }
 
 blog_route.post("/create",getauth,async(req,res)=>{
+    
     try{
-       
+        console.log("abhay");
         const {tittle,content,image}=req.body;
         
         if(tittle &&content){
     const user = req.user_id;
             const new_blog = await  blog.create({tittle,content,image, user});
-          console.log(req.body);
+        //   console.log(req.body);
             res.json({"name":"abhay"});
         }
         else res.status(202).json({err})  
@@ -43,12 +44,31 @@ blog_route.get("/find",getauth,async (req,res)=>{
     
     })
 
+    blog_route.get("/user/find",getauth,async (req,res)=>{
+    try{
+        return res.json({id:req.user_id})
+    }
+      catch{(err)=>res.json({msg:err}) } 
+      
+        
+        })
+
     blog_route.delete("/del/:id",getauth,async (req,res)=>{
-    console.log("arrived at delete");
+    console.log("abhay");
+    console.log("abhay");
       try{
+        console.log("abhay");
+        const selected_blog =await blog.findById(req.params.id);
+        console.log(selected_blog.user);
+        console.log(req.user_id);
+        if(selected_blog.user!=req.user_id) {
+           res.status(202).json({msg:"unauthorized"});
+        }
+        else{
          const delete_blog = await blog.findByIdAndDelete (req.params.id);
        if(!delete_blog)return res.json({error:"blog not found"});
        else return res.json(delete_blog)
+        }
     }
     
     
